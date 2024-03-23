0516534019fd341fafe2e96a10bfcd98d08df95f
Geovani Luiz de Moura <glmoura@apoioprodesp.sp.gov.br>
2024-03-23 13:22:46 -0300

    Atualizando

--- CrudBasico/CrudBasico/Scripts/cliente/index.js
+++ CrudBasico/CrudBasico/Scripts/cliente/index.js
@@ -1,31 +1,6 @@
 ﻿$(window).on("load", function () {
-
-	//Incluir
-	$("#clienteIncluir").on("click", function () {
-		window.location.href = "/Cliente/Create"
-	});
-
-	//Editar
-	$(".clienteEditar").on("click", function () {
-		var Id = $(this).data('id');
-		window.location.href = "/Cliente/Edit/" + Id
-	});
-
-	//Detalhes
-	$(".clienteDetalhes").on("click", function () {
-		var Id = $(this).data('id');
-		window.location.href = "/Cliente/Details/" + Id
-	});
-
-	//Excluir
-	$(".clienteExcluir").on("click", function () {
-		var Id = $(this).data('id');
-		if (confirm(`Deseja realmente excluir o id: ${Id}?`)) {
-			window.location.href = "/Cliente/Delete/" + Id
-		}
-	});	
-
-
+	
+	//Ignora que é do template
 	new DataTable('#tabelaCliente', {
 		info: false,
 		ordering: false,
